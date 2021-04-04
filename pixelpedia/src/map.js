3e755ad9a433d9c0ac172375d29073033cb08615
juneadkhan <junead@live.unc.edu>
2021-04-04 16:14:55 -0400

    Implemented SignIn with Firebase

--- pixelpedia/src/map.js
+++ pixelpedia/src/map.js
@@ -1,115 +1,118 @@
 import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from 'mapbox-gl/dist/mapbox-gl-csp';
 import MapboxWorker from 'worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker';
- 
+
 mapboxgl.workerClass = MapboxWorker;
 mapboxgl.accessToken = 'pk.eyJ1IjoianVuZWFkazI3IiwiYSI6ImNrbXpvcml4ajA0OXkydm8yODFmN3dtemgifQ.YacZnbSOmX93UZJMH7WLKg';
- 
+
 const Map = () => {
-const mapContainer = useRef();
-const [lng, setLng] = useState(-79.055847);
-const [lat, setLat] = useState(35.913200);
-const [zoom, setZoom] = useState(12);
- 
-useEffect(() => {
-const map = new mapboxgl.Map({
-container: mapContainer.current,
-style: 'mapbox://styles/juneadk27/ckn10m3z11mxg17loh9k451jr',
-center: [lng, lat],
-zoom: zoom
 
-});
-var geocoder = new MapboxGeocoder({ // Initialize the geocoder
-    accessToken: mapboxgl.accessToken, // Set the access token
-    mapboxgl: mapboxgl, // Set the mapbox-gl instance
-    marker: false, // Do not use the default marker style
-    placeholder: 'Search for places in North Carolina', // Placeholder text for the search bar
-    bbox: [-79.5657, 35.4682, -77.2503 , 36.2836 ], // Boundary for The Triangle
-    proximity: {
-      longitude: -79.055847,
-      latitude: 35.913200
-    } // Coordinates of Chapel Hill
-  });
 
-  map.addControl(geocoder);
+  const mapContainer = useRef();
+  const [lng, setLng] = useState(-79.055847);
+  const [lat, setLat] = useState(35.913200);
+  const [zoom, setZoom] = useState(12);
 
-  // After the map style has loaded on the page,
-// add a source layer and default styling for a single point
-map.on('load', function() {
-    map.addSource('single-point', {
-      type: 'geojson',
-      data: {
-        type: 'FeatureCollection',
-        features: []
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/juneadk27/ckn10m3z11mxg17loh9k451jr',
+      center: [lng, lat],
+      zoom: zoom
+
+    });
+    var geocoder = new MapboxGeocoder({ // Initialize the geocoder
+      accessToken: mapboxgl.accessToken, // Set the access token
+      mapboxgl: mapboxgl, // Set the mapbox-gl instance
+      marker: false, // Do not use the default marker style
+      placeholder: 'Search for places in North Carolina', // Placeholder text for the search bar
+      bbox: [-79.5657, 35.4682, -77.2503, 36.2836], // Boundary for The Triangle
+      proximity: {
+        longitude: -79.055847,
+        latitude: 35.913200
+      } // Coordinates of Chapel Hill
+    });
+
+    map.addControl(geocoder);
+
+    // After the map style has loaded on the page,
+    // add a source layer and default styling for a single point
+    map.on('load', function () {
+      map.addSource('single-point', {
+        type: 'geojson',
+        data: {
+          type: 'FeatureCollection',
+          features: []
+        }
+      });
+
+      map.addLayer({
+        id: 'point',
+        source: 'single-point',
+        type: 'circle',
+        paint: {
+          'circle-radius': 10,
+          'circle-color': '#448ee4'
+        }
+      });
+
+      // Listen for the `result` event from the Geocoder
+      // `result` event is triggered when a user makes a selection
+      //  Add a marker at the result's coordinates
+      geocoder.on('result', function (e) {
+        map.getSource('single-point').setData(e.result.geometry);
+      });
+    });
+
+
+
+    map.on('click', function (e) {
+
+      var features = map.queryRenderedFeatures(e.point, {
+        layers: ['pixelpediatest'] // replace this with the name of the layer
+      });
+
+      if (!features.length) {
+        return;
       }
-    });
-  
-    map.addLayer({
-      id: 'point',
-      source: 'single-point',
-      type: 'circle',
-      paint: {
-        'circle-radius': 10,
-        'circle-color': '#448ee4'
-      }
-    });
-  
-    // Listen for the `result` event from the Geocoder
-    // `result` event is triggered when a user makes a selection
-    //  Add a marker at the result's coordinates
-    geocoder.on('result', function(e) {
-      map.getSource('single-point').setData(e.result.geometry);
-    });
-  });
 
-  
+      var feature = features[0];
+      console.log(feature)
 
-  map.on('click', function(e) {
-    var features = map.queryRenderedFeatures(e.point, {
-      layers: ['pixelpediatest'] // replace this with the name of the layer
+      var popup = new mapboxgl.Popup({ offset: [0, -15] })
+        .setLngLat(feature.geometry.coordinates)
+        .setHTML('<h3>' + feature.properties.Name + '</h3><p>' + feature.properties.Description + '</p>'
+          + '<img src="https://upload.wikimedia.org/wikipedia/commons/e/ea/Bell_tower_in_Chapel_Hill_%28cropped%29.jpg" width="200" height="200">'
+          + '<img src="https://www.unc.edu/wp-content/uploads/2019/04/008919_BellTowerClimb0178.jpg" width="200" height="200">'
+          + '<img src="https://i.pinimg.com/originals/8f/20/91/8f209105e938d26a19780f97dfc825b8.jpg" width="200" height="200">'
+        )
+        .addTo(map);
     });
-  
-    if (!features.length) {
-      return;
-    }
-  
-    var feature = features[0];
-    console.log(feature)
-  
-    var popup = new mapboxgl.Popup({ offset: [0, -15] })
-      .setLngLat(feature.geometry.coordinates)
-      .setHTML('<h3>' + feature.properties.Name + '</h3><p>' + feature.properties.Description + '</p>'
-       + '<img src="https://upload.wikimedia.org/wikipedia/commons/e/ea/Bell_tower_in_Chapel_Hill_%28cropped%29.jpg" width="200" height="200">'
-       + '<img src="https://www.unc.edu/wp-content/uploads/2019/04/008919_BellTowerClimb0178.jpg" width="200" height="200">'
-       + '<img src="https://i.pinimg.com/originals/8f/20/91/8f209105e938d26a19780f97dfc825b8.jpg" width="200" height="200">'
-       )
-      .addTo(map);
-  });
- 
-  
-/*
-var marker = new mapboxgl.Marker() // initialize a new marker
-  .setLngLat([-79.055847, 35.913200]) // Marker [lng, lat] coordinates
-  .addTo(map); // Add the marker to the map
-  */
 
-map.on('move', () => {
-setLng(map.getCenter().lng.toFixed(4));
-setLat(map.getCenter().lat.toFixed(4));
-setZoom(map.getZoom().toFixed(2));
-});
- 
-return () => map.remove();
-}, []);
- 
-return (
-<div>
-<div className="sidebar">
-Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-</div>
-<div className="map-container" ref={mapContainer} />
-</div>
-);
+
+    /*
+    var marker = new mapboxgl.Marker() // initialize a new marker
+      .setLngLat([-79.055847, 35.913200]) // Marker [lng, lat] coordinates
+      .addTo(map); // Add the marker to the map
+      */
+
+    map.on('move', () => {
+      setLng(map.getCenter().lng.toFixed(4));
+      setLat(map.getCenter().lat.toFixed(4));
+      setZoom(map.getZoom().toFixed(2));
+    });
+
+    return () => map.remove();
+  }, []);
+
+  return (
+    <div>
+      <div className="sidebar">
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+      </div>
+      <div className="map-container" ref={mapContainer} />
+    </div>
+  );
 };
 
 export default Map;
--- pixelpedia/src/navigation.js
+++ pixelpedia/src/navigation.js
@@ -1,9 +1,10 @@
-import { Navbar, Nav, Form, FormControl, Button, Dropdown, ButtonGroup, Modal, Container} from 'react-bootstrap';
+import { Navbar, Nav, Form, FormControl, Button, Dropdown, ButtonGroup, Modal, Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import { LinkContainer } from 'react-router-bootstrap'
 import React, { useEffect, useState } from "react";
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import firebase from 'firebase';
+
 
 
 // Configure FirebaseUI.
@@ -16,19 +17,26 @@ const firebaseConfig = {
   appId: "1:30869197092:web:bc6eb12e71843cd45723b1"
 };
 
+// The component's Local state.
+let state = {
+  isSignedIn: false // Local signed-in state.
+};
+
+
+
 // Configure FirebaseUI.
 const uiConfig = {
   // Popup signin flow rather than redirect flow.
   signInFlow: 'popup',
   // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-  signInSuccessUrl: '/signedIn',
+  signInSuccessUrl: '/',
   // We will display Google and Facebook as auth providers.
   signInOptions: [
-      firebase.auth.EmailAuthProvider.PROVIDER_ID,
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      firebase.auth.GithubAuthProvider.PROVIDER_ID
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    firebase.auth.GithubAuthProvider.PROVIDER_ID
 
   ]
 };
@@ -44,36 +52,130 @@ function Navigation() {
 
   const [darkTheme, setDarkTheme] = React.useState(false)
 
+  function handleClick() {
+    setDarkTheme(prevTheme => !prevTheme)
+    document.getElementById("theme").className = darkTheme ? 'light-theme' : 'dark-theme'
+  }
 
-  return (
-    <>
-      <div className= {darkTheme ? 'dark-theme' : 'light-theme'}>
-      <Navbar>
-        <Navbar.Brand href="#home" style={{color: darkTheme ? 'white' : 'black'}}>Pixelpedia</Navbar.Brand>
-        <Nav className="mr-auto">
-        </Nav>
-        <Button variant={darkTheme ? 'outline-warning' : 'outline-secondary'} onClick={() => setDarkTheme(prevTheme => !prevTheme)}>{darkTheme ? '🌚' : '☀️'}</Button>
-        <Button variant="outline-info" onClick={handleShow}>Sign In</Button>
-      </Navbar>
-      </div>
+  const [user, setUser] = useState(null);
 
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Dialog>
-          <Modal.Header closeButton>
-            <Modal.Title>Sign In</Modal.Title>
-          </Modal.Header>
+  useEffect(() => {
+    // no need for ref here
+    const unsubscribeFromAuth = firebase.auth().onAuthStateChanged((user) => {
 
-          <Modal.Body>
-            <Container>
-              <h2 style={{ textAlign: 'center' }}>Welcome to Pixelpedia</h2>
-              <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
-            </Container>
-          </Modal.Body>
-        </Modal.Dialog>
-      </Modal>
+      if (user) {
+        console.log("The user is logged in");
+        state.isSignedIn = true
+      } else {
+        console.log("The user is not logged in");
+        state.isSignedIn = false
+      }
 
-    </>
-  );
+      setUser(user);
+      console.log(user)
+      // state.isSignedIn = !state.isSignedIn;
+      console.log(firebase.auth().currentUser)
+      console.log(state.isSignedIn)
+
+    })
+
+    return () => {
+      if (!state.isSignedIn){
+        unsubscribeFromAuth();
+      }
+    }
+  }, []);
+
+  function handleSignOut(){
+    firebase.auth().signOut()
+    state.isSignedIn = false;
+    console.log(firebase.auth().currentUser)
+    console.log(state.isSignedIn)
+
+  }
+
+  
+  // Listen to the Firebase Auth state and set the local state.
+  function componentDidMount(){
+    this.unregisterAuthObserver = firebase.auth().onAuthStateChanged( (user) => this.setState({ isSignedIn: !!user } ));
+  }
+  
+
+  // Make sure we un-register Firebase observers when the component unmounts.
+  function componentWillUnmount() {
+    this.unregisterAuthObserver();
+  }
+
+
+  if (!state.isSignedIn) {
+
+    return (
+      <>
+        <div className={darkTheme ? 'dark-theme' : 'light-theme'}>
+          <Navbar>
+            <Navbar.Brand href="#home" style={{ color: darkTheme ? 'white' : 'black' }}>Pixelpedia</Navbar.Brand>
+            <Nav className="mr-auto">
+            </Nav>
+            <Button variant={darkTheme ? 'outline-warning' : 'outline-secondary'} onClick={handleClick}>{darkTheme ? '🌚' : '☀️'}</Button>
+            <Button variant="outline-info" onClick={handleShow}>Sign In</Button>
+          </Navbar>
+        </div>
+
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Dialog>
+            <Modal.Header closeButton>
+              <Modal.Title>Sign In</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body>
+              <Container>
+                <h2 style={{ textAlign: 'center' }}>Welcome to Pixelpedia</h2>
+                <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+              </Container>
+            </Modal.Body>
+          </Modal.Dialog>
+        </Modal>
+
+      </>
+    );
+
+  } else {
+
+    return (
+      <>
+        <div className={darkTheme ? 'dark-theme' : 'light-theme'}>
+          <Navbar>
+            <Navbar.Brand href="#home" style={{ color: darkTheme ? 'white' : 'black' }}>Pixelpedia</Navbar.Brand>
+            <Nav className="mr-auto">
+            </Nav>
+            <Button variant={darkTheme ? 'outline-warning' : 'outline-secondary'} onClick={handleClick}>{darkTheme ? '🌚' : '☀️'}</Button>
+            <Button variant="outline-info" onClick={handleShow}>Signed in as {firebase.auth().currentUser.displayName}</Button>
+          </Navbar>
+        </div>
+
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Dialog>
+            <Modal.Header closeButton>
+              <Modal.Title>Already Signed In</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body>
+              <Container>
+                <h3 style={{ textAlign: 'center' }}>Hey, {firebase.auth().currentUser.displayName} 👋</h3>
+                <br></br>
+                <Button variant="outline-danger" onClick={handleSignOut}>Sign Out</Button>                
+              </Container>
+            </Modal.Body>
+          </Modal.Dialog>
+        </Modal>
+
+      </>
+    );
+
+  }
+
+
+
 }
 
 export default Navigation;
