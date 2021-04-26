d93414801313543d89c519e5d253e227b835f7fa
juneadkhan <junead@live.unc.edu>
2021-04-26 16:29:16 -0400

    Implemented Weather API via Axios

--- pixelpedia/src/map.js
+++ pixelpedia/src/map.js
@@ -3,10 +3,35 @@ import mapboxgl, { Popup } from 'mapbox-gl/dist/mapbox-gl-csp';
 import MapboxWorker from 'worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker';
 import ReactDOM from 'react-dom';
 import { Carousel, Container, Row, Col } from 'react-bootstrap';
-import { Button, IconButton } from '@material-ui/core/';
+import { Button, IconButton, Paper } from '@material-ui/core/';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import axios from 'axios';
 
+
+export async function getWeatherIcon(lat, lng) {
+
+  const result = await axios({
+    method: 'get',
+    url: 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lng + '&exclude=&appid=f36e366463c261985623b04441c510f8',
+  });
+
+  console.log("DaTA: ", result.data['current']['weather'][0]['icon'])
+  return result.data['current']['weather'][0]['icon']
+
+};
+
+export async function getTemperature(lat, lng) {
+
+  const result = await axios({
+    method: 'get',
+    url: 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lng + '&exclude=&appid=f36e366463c261985623b04441c510f8&units=metric',
+  });
+
+  console.log("DaTA: ", Math.round(result.data['current']['temp']))
+  return Math.round(result.data['current']['temp'])
+
+};
 
 mapboxgl.workerClass = MapboxWorker;
 mapboxgl.accessToken = 'pk.eyJ1IjoianVuZWFkazI3IiwiYSI6ImNrbXpvcml4ajA0OXkydm8yODFmN3dtemgifQ.YacZnbSOmX93UZJMH7WLKg';
@@ -45,7 +70,7 @@ const Map = () => {
       mapboxgl: mapboxgl, // Set the mapbox-gl instance
       marker: false, // Do not use the default marker style
       placeholder: 'Find photo spots around...', // Placeholder text for the search bar
-  
+
     });
 
     map.addControl(geocoder);
@@ -85,7 +110,7 @@ const Map = () => {
 
 
 
-    map.on('click', function (e) {
+    map.on('click', async function (e) {
 
       var features = map.queryRenderedFeatures(e.point, {
         layers: ['pixelpediatest'] // replace this with the name of the layer
@@ -98,6 +123,13 @@ const Map = () => {
       var feature = features[0];
       var latDisp = feature.geometry.coordinates[1];
       var lngDisp = feature.geometry.coordinates[0];
+
+      var icon = await getWeatherIcon(latDisp, lngDisp)
+      var iconURL = "https://openweathermap.org/img/wn/" + icon + ".png"
+      var temp = await getTemperature(latDisp, lngDisp)
+
+
+
 
       console.log("FEATURE", feature)
 
@@ -115,14 +147,19 @@ const Map = () => {
               flexDirection: "row",
               justifyContent: "flex-start"
             }}>
-              <h3 className = "propertyName">{feature.properties.Name}</h3>
+              <h3 className="propertyName">{feature.properties.Name}</h3>
               <p>{feature.properties.Description}</p>
+
               <Container>
                 <Row>
                   <Button style={{ backgroundColor: "#23272b", color: "white" }} variant="contained" block href={"https://maps.google.com/?q=" + latDisp + "," + lngDisp} target="_blank" disableElevation>
                     <svg style={{ marginRight: "0.5rem" }} aria-hidden="true" width="18" height="18" viewBox="0 0 18 18"><path d="M16.51 8H8.98v3h4.3c-.18 1-.74 1.48-1.6 2.04v2.01h2.6a7.8 7.8 0 002.38-5.88c0-.57-.05-.66-.15-1.18z" fill="#4285F4"></path><path d="M8.98 17c2.16 0 3.97-.72 5.3-1.94l-2.6-2a4.8 4.8 0 01-7.18-2.54H1.83v2.07A8 8 0 008.98 17z" fill="#34A853"></path><path d="M4.5 10.52a4.8 4.8 0 010-3.04V5.41H1.83a8 8 0 000 7.18l2.67-2.07z" fill="#FBBC05"></path><path d="M8.98 4.18c1.17 0 2.23.4 3.06 1.2l2.3-2.3A8 8 0 001.83 5.4L4.5 7.49a4.77 4.77 0 014.48-3.3z" fill="#EA4335"></path></svg>
                     Open in Google Maps
                 </Button>
+                  <Paper variant="outlined" disabled style={{marginLeft: "0.5rem", paddingRight:"0.5rem", paddingLeft:"0.5rem", height:"50px"}}>
+                  <img className="img-fluid" src={iconURL} style={{ width: "25px", height: "25px" }} />
+                  <p style={{fontSize:"12px"}}>{temp + "°C"}</p></Paper>
+
 
                   { /* 
                 <IconButton aria-label="delete">
